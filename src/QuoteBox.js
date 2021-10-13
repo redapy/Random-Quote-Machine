9fda1c0e34f4d196451c5fb3578add2fd5df56d3
redapy <baha.redaaaa@gmail.com>
2021-10-13 14:03:40 +0100

    handle errors

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,6 +1,18 @@
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
+const variants = {
+    initial: {
+        x: '400px',
+        opacity:0
+    },
+    animate: {
+        x:0,
+        opacity: 1,
+        transition: {type: 'tween', duration:1.5}
+    }
+}
+
 
 const QuoteBox = () => {
     var colors = [
@@ -21,6 +33,7 @@ const QuoteBox = () => {
     const [quote, setQuote] = useState([]);
     const [color, setColor] = useState('#73A857');
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(false);
 
     const handleClick = () => {
         fetch("https://type.fit/api/quotes")
@@ -28,9 +41,12 @@ const QuoteBox = () => {
         .then( data =>
             setQuote(data[Math.floor(Math.random()*data.length)])
         )
+        .catch(e => {
+            setError(true)
+            setIsPending(false)
+        })
 
         setColor(colors[Math.floor(Math.random()*colors.length)])
-        
     }
 
     useEffect(() => {
@@ -40,25 +56,34 @@ const QuoteBox = () => {
             {
                 setQuote(data[Math.floor(Math.random()*data.length)]);
                 setIsPending(false)
-            }
-        )
+            })
+            .catch(e => {
+                setError(true)
+                setIsPending(false)
+            })
     }, []);
 
     return ( 
         <div style={{backgroundColor: color}} className="wrapper">
-            <motion.div id="quote-box"
-             initial={{x: '100vw'}}
-             animate={{x:0}}
-             transition={{delay: 0.5, type: 'tween', duration:2}}
+            <motion.div 
+                id="quote-box"
+                variants={variants}
+                initial="initial"
+                animate="animate"
             >
+                {error && <div>couldn't fetch the data</div>}
                 {isPending && <div>Loading...</div>}
                 <h1 style={{color: color}} id="text">{quote.text}</h1>
                 <p id="author">-by {quote.author}</p>
                 <div className="flex">
-                    <a id="tweet-quote" class="btn-floating btn btn-tw" type="button" role="button" title="Share on twitter"
-                    href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fparse.com"
-                    rel="noopener"
-                    target="_blank">
+                    <a 
+                        id="tweet-quote" 
+                        type="button" 
+                        role="button" 
+                        title="Share on twitter"
+                        href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fparse.com"
+                        rel="noreferrer"
+                        target="_blank">
                     <i class="fab fa-2x fa-twitter"> Tweet it</i>
                     </a>
                     <motion.button style={{backgroundColor: color}} id="new-quote" onClick={handleClick} 
@@ -67,6 +92,7 @@ const QuoteBox = () => {
                         textShadow: "0px 0px 8px rgb(255,255,255)",
                         boxShadow: "0px 0px 8px rgb(255,255,255)",
                       }}
+                      
                     >
                     New Quote
                     </motion.button>
